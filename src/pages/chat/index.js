74b1d220b3a3339d1a245ee726586e986a440a43
GabrielCarreiro <63800998+GabrielCarreiro@users.noreply.github.com>
2020-10-10 23:39:13 -0300

    Atualização chat

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,82 +1,121 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import {Text, TextInput, View, TouchableOpacity} from 'react-native';
-import { ViewMessage, MessageText} from './styles'
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { Text, ScrollView } from 'react-native';
+import { Container, MessageView, MessageText, MessageTextUser, MessageSendView, MessageSendInput, MessagenSendButton } from './styles'
+import AsyncStorage from '@react-native-community/async-storage';
 
 import firebase from 'firebase';
 import 'firebase/firestore';
 
-const Chat = () =>{
-    const [messageUser, setMessageUser] = useState();
-    const [allMessage, setAllMessage] = useState([]);
-    console.log(messageUser)
+const Chat = () => {
+  const [messageUser, setMessageUser] = useState();
+  const [allMessage, setAllMessage] = useState([]);
+  const [userAuth, setUserAuth] = useState();
+  const scrollViewRef = useRef();
 
-    const loadMessage = useCallback(
-        async () => {
-          try {
-            const response = await firebase.firestore().collection('help-chat').get();
-            const temp = [];
-            response.forEach(doc => {
-              // console.log(doc.id, '=>', doc.data());
-              temp.push({id: doc.id, ...doc.data()});
-            })
-            setAllMessage(temp);
+  function ordemDecrescente(a, b) {
+    return a.date > b.date;
+  }
 
-          } catch (error) {
-            console.log('error loadMessage', error);
-          }
-      },[]);
-   
-    useEffect(() =>{
-        loadMessage()
-    },[])
 
-    const onTarefasChanged = useCallback(snap => {
-        const data = snap.docs.map(doc => ({id: doc.id, ...doc.data()}));
+  const loadMessage = useCallback(
+    async () => {
+      try {
+        const response = await firebase.firestore().collection('help-chat').get();
+        const temp = [];
+        response.forEach(doc => {
+          // console.log(doc.id, '=>', doc.data());
+          temp.push({ id: doc.id, ...doc.data() });
+        })
+        temp.sort(ordemDecrescente);
+        setAllMessage(temp);
 
-        setAllMessage(data);
-        loadMessage();
-      }, []);
+      } catch (error) {
+        console.log('error loadMessage', error);
+      }
+    }, []);
 
-    useEffect(() => {
-        const unsubscribe = firebase.firestore().collection('help-chat').onSnapshot(onTarefasChanged);
-        return () => unsubscribe();
-      }, [])
-
-    console.log(allMessage)
-    
-    const handleAddTask =  async () => {
-          try {
-            await firebase.firestore().collection('help-chat').add({
-             message: messageUser
-            });
-            setMessageUser("");
-            loadMessage();
-          } catch (error) {
-            console.log("error handleAddTask:", error);
-          }
+  const loadUserAuth = async () => {
+    try {
+      const user = JSON.parse(await AsyncStorage.getItem('@HELP:user'));
+      setUserAuth(user)
+    } catch (error) {
+      console.log("Erro ao carregar o usuario", error)
     }
-/*  */
+  }
 
-    return (
-        <>  
-            <ViewMessage>
-                {allMessage.map(mess =>{
-                    return(
-                    <MessageText key={mess.id}> {mess.message}</MessageText>
-                    )
-                })}
-            </ViewMessage>
-            <View style={{justifyContent:"center", flex: 1, alignItems:"center"}}>
-                <TextInput  style={{width:200, backgroundColor:"red", color: "#fff"}} 
-                            placeholder="Digite aqui"
-                            value={messageUser} onChangeText={text => setMessageUser(text)}> 
-                </TextInput>
-                <TouchableOpacity onPress={() => handleAddTask()}>
-                    <Text> Enviar </Text>
-                </TouchableOpacity>
-            </View>
-        </>
-    )
+  useEffect(() => {
+    loadMessage()
+    loadUserAuth()
+  }, [])
+
+  const onTarefasChanged = useCallback(snap => {
+    const data = snap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    setAllMessage(data);
+    loadMessage();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = firebase.firestore().collection('help-chat').onSnapshot(onTarefasChanged);
+    return () => unsubscribe();
+  }, [])
+
+  console.log(allMessage)
+
+  const handleAddTask = async () => {
+
+    if (!messageUser) return;
+
+    try {
+      await firebase.firestore().collection('help-chat').add({
+        message: messageUser,
+        date: new Date,
+        user: userAuth.id,
+        name: userAuth.nome
+      });
+      setMessageUser("");
+      loadMessage();
+    } catch (error) {
+      console.log("error handleAddTask:", error);
+    }
+  }
+
+
+  const loadAllMessage = useCallback(() => {
+    return allMessage.map(mess => {
+      if (mess.user === userAuth.id) {
+        return <MessageTextUser key={mess.id}> {mess.name} {'\n'} {mess.message}</MessageTextUser>
+      } else {
+        return <MessageText key={mess.id}> {mess.name} {'\n'} {mess.message} </MessageText>
+      }
+    })
+  }, [allMessage])
+
+
+  console.log(userAuth)
+
+  return (
+    <>
+      <Container>
+        <MessageView>
+          < ScrollView ref={scrollViewRef}
+            onContentSizeChange={() => scrollViewRef.current.scrollToEnd({ animated: true })}>
+            {loadAllMessage()}
+          </ScrollView>
+        </MessageView>
+        <MessageSendView >
+          <MessageSendInput style={{ color: "#fff" }}
+            placeholder="Digite aqui"
+            placeholderTextColor="white"
+            value={messageUser} onChangeText={text => setMessageUser(text)}>
+          </MessageSendInput >
+          <MessagenSendButton onPress={() => handleAddTask()}>
+            <Text style={{ color: "#fff", fontWeight: "bold" }}> Enviar </Text>
+          </MessagenSendButton>
+        </MessageSendView>
+      </Container>
+    </>
+  )
 };
 
 export default Chat;
--- src/pages/chat/styles.js
+++ src/pages/chat/styles.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components/native';
 
-export const ViewMessage = styled.View`
+export const Container = styled.View`
+    background-color: rgb(64,62,63);
+    height: 100%;
+`;
+
+export const MessageView = styled.View`
     padding:10px 20px;
+    background-color: rgb(64,62,63);
+    height: 90%;
 `;
 
 export const MessageText = styled.Text`
@@ -9,5 +16,47 @@ export const MessageText = styled.Text`
     margin: 10px;
     padding: 5px;
     color: #fff;
-    font-size: 18px;
+    font-size: 16px;
+    border-radius: 8px;
 `;
+
+export const MessageSendView = styled.View`
+    background-color: rgb(64,62,63);
+    justify-content: center;
+    align-items: flex-end;
+    height: 48px;
+    flex-direction: row;
+    width: 100%;
+`;
+
+export const MessageSendInput = styled.TextInput`
+    background-color: rgb(64,62,63);
+    border-radius: 8px;
+    border: 1px solid rgba(239, 80, 40, 131);
+    width: 250px;
+    height: 40px;
+    padding: 10px;
+    margin-bottom: 5px;
+`;
+
+export const MessagenSendButton = styled.TouchableOpacity`
+    background-color: rgba(239, 80, 40, 131);
+    height: 43px;
+    width: 80px;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10px;
+    border-radius: 8px;
+    margin-bottom: 2px;
+`;
+
+export const MessageTextUser = styled.Text`
+    background-color: rgba(239, 80, 40, 131);
+    margin: 10px;
+    padding: 5px;
+    color: #fff;
+    font-size: 16px;
+    border-radius: 8px;
+    text-align: right;
+`;
+
